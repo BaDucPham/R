900d26c27a08ea3d4a3bdf2c47e8077500144f4b
Ba Duc Pham <89392909+BaDucPham@users.noreply.github.com>
2023-06-26 13:31:39 +0200

    Add files via upload

---
+++ rstudio/td1.R
@@ -0,0 +1,26 @@
+test <- 0
+autre <- 3
+5 * autre
+numero <- 16
+donnee <- 23
+valeur <- 7 * numero + donnee * 4
+print(valeur)
+
+
+vecteur_1 <- 1:5
+vecteur_2 <- seq(from = 2, to = 14, by =2)
+print(vecteur_2[2])
+
+nom <- "Modélisation"
+rien <- NA
+texte <- c("a","b","c")
+vrai_ou_faux <- c(TRUE,FALSE)
+print(vrai_ou_faux[1] & vrai_ou_faux[2])
+
+? is.numeric()
+test <- "53"
+valeur <- as.numeric(test)
+aucun_texte <- NA_character_
+un_entier <- as.integer(542.423)
+as.character(4325.423)
+
